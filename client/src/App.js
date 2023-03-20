0131ac588f61935ecc32b9221f3c01822f08a51e
mosesochacha <ochachamoses443@@gmail.com>
2023-03-20 18:51:56 +0300

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import Navbar from './components/navbar';
 import './App.css';
+import { useState , useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+const [restaurants, setRestaurants] = useState()
+
+ useEffect(()=>{
+   axios.get("http://localhost:3000/restaurants")
+   .then((res)=>{
+    setRestaurants(res.data)
+   })
+
+ },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
     </div>
   );
 }
---
+++ client/src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+export default function Navbar(){
+
+
+  return(
+    <div>
+      <nav className="navbar bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="M">Navbar</a>
+  </div>
+</nav>
+
+    </div>
+  )
+}
